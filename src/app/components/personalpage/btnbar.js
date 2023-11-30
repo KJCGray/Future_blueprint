4be42897eab7b7cad59c37a49495b7039a5d2748
PeiRong13 <patty10322213@gmail.com>
2023-12-01 00:36:35 +0800

    userdata

---
+++ src/app/components/personalpage/btnbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const btnbar = () => {
+  return (
+    <div style={{ width: '50%', background: '#f0f0f0', padding: '1rem' }}>
+      
+    </div>
+  )
+}
+
+export default btnbar;
--- src/app/personal/page.js
+++ src/app/personal/page.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import Userdata from "../components/personalpage/userdata";
+import Btnbar from "../components/personalpage/btnbar";
 import SearchBar from '../components/mainpage/searchbar';
 
 const page = () => {
   return (
-    <div className="w-screen h-screen m-0 bg-white">
-        <SearchBar/>
-        <Userdata/>
+    <div className='w-screen h-screen m-0 bg-white'>
+      <SearchBar/>
+      <Userdata/>
+      <div className='items-center'>
+        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
+          <div className='font-semibold text-red-950'>
+            證  照
+          </div>
+        </div>     
+        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
+          <div className='font-semibold text-red-950'>
+            語  言
+          </div>
+        </div>   
+        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
+          <div className='font-semibold text-red-950'>
+            學  歷
+          </div>
+        </div>   
+        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
+          <div className='font-semibold text-red-950'>
+            專業能力
+          </div>
+        </div>   
+        <div className='flex justify-center w-40 my-2 bg-orange-200 rounded'>
+          <div className='font-semibold text-red-950'>
+            個人化工作推薦
+          </div>
+        </div>                                   
+      </div>               
     </div>
   )
 }
