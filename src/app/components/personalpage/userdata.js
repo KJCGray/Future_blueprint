3c03a7e44fb46775b33791cad8987c6b3836aa77
PeiRong13 <patty10322213@gmail.com>
2023-12-01 00:40:39 +0800

    userdata

--- src/app/components/personalpage/userdata.js
+++ src/app/components/personalpage/userdata.js
@@ -5,7 +5,35 @@ const userdata = () => {
   return (
     <div>
       <AccountCircleIcon sx={{ fontSize: 40 }} className='mx-2.5' />
+      <div className='items-center'>
+        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
+          <div className='font-semibold text-red-950'>
+            證  照
+          </div>
+        </div>     
+        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
+          <div className='font-semibold text-red-950'>
+            語  言
+          </div>
+        </div>   
+        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
+          <div className='font-semibold text-red-950'>
+            學  歷
+          </div>
+        </div>   
+        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
+          <div className='font-semibold text-red-950'>
+            專業能力
+          </div>
+        </div>   
+        <div className='flex justify-center w-40 my-2 bg-orange-200 rounded'>
+          <div className='font-semibold text-red-950'>
+            個人化工作推薦
+          </div>
+        </div>                                   
+      </div>        
     </div>
+    
   )
 }
 
--- src/app/personal/page.js
+++ src/app/personal/page.js
@@ -7,34 +7,7 @@ const page = () => {
   return (
     <div className='w-screen h-screen m-0 bg-white'>
       <SearchBar/>
-      <Userdata/>
-      <div className='items-center'>
-        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
-          <div className='font-semibold text-red-950'>
-            證  照
-          </div>
-        </div>     
-        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
-          <div className='font-semibold text-red-950'>
-            語  言
-          </div>
-        </div>   
-        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
-          <div className='font-semibold text-red-950'>
-            學  歷
-          </div>
-        </div>   
-        <div className='flex justify-center w-20 my-2 bg-orange-200 rounded'>
-          <div className='font-semibold text-red-950'>
-            專業能力
-          </div>
-        </div>   
-        <div className='flex justify-center w-40 my-2 bg-orange-200 rounded'>
-          <div className='font-semibold text-red-950'>
-            個人化工作推薦
-          </div>
-        </div>                                   
-      </div>               
+      <Userdata/>             
     </div>
   )
 }
