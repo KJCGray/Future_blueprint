19fd8942a8827bbf80b6615e1dee3eba2d04ff46
Yvonne <a0905001923@gmail.com>
2023-12-12 23:06:40 +0800

    change

--- frontend/src/app/components/register/signup.js
+++ frontend/src/app/components/register/signup.js
@@ -3,7 +3,8 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useRouter } from "next/navigation";
-import axios from "axios";mmi
+import { useRef } from "react";
+import axios from "axios";
 import Swal from "sweetalert2";
 
 function Signup() {
