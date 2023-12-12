937575566528fb1592cca9bc3b67689fbf81449d
Yvonne <a0905001923@gmail.com>
2023-12-12 16:26:22 +0800

    new frontend

--- frontend/src/app/components/personalpage/accountsetting.js
+++ frontend/src/app/components/personalpage/accountsetting.js
@@ -1,44 +1,72 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-const userdata = () => {
-    
+const Accountsetting = () => {
+    const [isName, setIsName] = useState(false);
+
+    const handleNameClick = () => {
+      setIsName(!isName);
+    }
+  
+    const [isMail, setIsMail] = useState(false);
+
+    const handleMailClick = () => {
+      setIsMail(!isMail);
+    }
+  
+    const [isEducational, setIsEducational] = useState(false);
+
+    const handleEducationalClick = () => {
+      setIsEducational(!isEducational);
+    }
+  
+    const [isPassword, setIsPassword] = useState(false);
+
+    const handlePasswordClick = () => {
+      setIsPassword(!isPassword);
+    }
+  
   return (
     <div className=' bg-orange-100 rounded-xl w-[600px] h-[400px]'>
-        <div className='flex flex-col justify-center mt-8 ml-20'>
+        <div className='flex flex-col w-3/5 pt-4 ml-16'>
+
+            <div className='flex items-center mt-5'>
+                <Button onClick={handleNameClick} 
+                className='flex items-center justify-center w-20 h-8 my-6 font-semibold text-yellow-900 bg-orange-200 rounded'>
+                姓   名
+                </Button>           
+                <div className='ml-8'>
+                    {isName ? '未輸入' : (<TextField id="standard-basic" label="Name" variant="standard"/>)}
+                </div>
+            </div> 
             <div className='flex items-center'>
-                <div className='flex items-center justify-center w-20 h-8 my-6 mr-10 bg-orange-200 rounded'>
-                    姓名
+                <Button onClick={handleMailClick} 
+                className='flex items-center justify-center w-20 h-8 my-6 font-semibold text-yellow-900 bg-orange-200 rounded'>
+                信   箱
+                </Button>           
+                <div className='ml-8'>
+                    {isMail ? '未輸入' : (<TextField id="standard-basic" label="Mail" variant="standard"/>)}
                 </div>
-                <div className='mb-5'>
-                    <TextField id="standard-basic" label="Name" variant="standard" />
-                </div>
-            </div>
+            </div> 
             <div className='flex items-center'>
-                <div className='flex items-center justify-center w-20 h-8 my-6 mr-10 bg-orange-200 rounded'>
-                    信箱
+                <Button onClick={handleEducationalClick} 
+                className='flex items-center justify-center w-20 h-8 my-6 font-semibold text-yellow-900 bg-orange-200 rounded'>
+                學   歷
+                </Button>           
+                <div className='ml-8'>
+                    {isEducational ? '未輸入' : (<TextField id="standard-basic" label="Educational" variant="standard"/>)}
                 </div>
-                <div className='mb-5'>
-                    <TextField id="standard-basic" label="Mail" variant="standard" />
-                </div>
-            </div>
+            </div> 
             <div className='flex items-center'>
-                <div className='flex items-center justify-center w-20 h-8 my-6 mr-10 bg-orange-200 rounded'>
-                    學歷
+                <Button onClick={handlePasswordClick} 
+                className='flex items-center justify-center w-20 h-8 my-6 font-semibold text-yellow-900 bg-orange-200 rounded'>
+                密   碼
+                </Button>           
+                <div className='ml-8'>
+                    {isPassword ? '未輸入' : (<TextField id="standard-basic" label="Password" variant="standard"/>)}
                 </div>
-                <div className='mb-5'>
-                    <TextField id="standard-basic" label="Educational" variant="standard" />
-                </div>
-            </div>
-            <div className='flex items-center'>
-                <div className='flex items-center justify-center w-20 h-8 my-6 mr-10 bg-orange-200 rounded'>
-                    密碼
-                </div>
-                <div className='mb-5'>
-                    <TextField id="standard-basic" label="Password" variant="standard" />
-                </div>
-            </div>
+            </div> 
 
         </div>
     </div>
@@ -46,4 +74,4 @@ const userdata = () => {
   )
 }
 
-export default userdata
+export default Accountsetting;
--- frontend/src/app/components/personalpage/userdata.js
+++ frontend/src/app/components/personalpage/userdata.js
@@ -1,43 +1,85 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import TextField from '@mui/material/TextField';
+const Userdata = () => {
+  const [isCer, setIsCer] = useState(false);
 
-const userdata = () => {
+  const handleCerClick = () => {
+    setIsCer(!isCer);
+  }
+
+  const [isLanguage, setIsLanguage] = useState(false);
+
+  const handleLanguageClick = () => {
+    setIsLanguage(!isLanguage);
+  }
+
+  const [isEducational, setIsEducational] = useState(false);
+
+  const handleEducationalClick = () => {
+    setIsEducational(!isEducational);
+  }
+
+  const [isMajor, setIsMajor] = useState(false);
+
+  const handleMajorClick = () => {
+    setIsMajor(!isMajor);
+  }
   return (
     <div className=' bg-orange-100 rounded-xl w-[600px] h-[400px]'>
-      <div className='flex flex-col items-center w-[200px] pt-4'>
-        <div className='mt-8'>
-          <AccountCircleIcon sx={{ fontSize: 40 }} />
-        </div>
-        <div className='flex items-center justify-center w-20 h-8 mx-5 my-3 bg-orange-200 rounded'>
-          <div className='font-semibold text-red-950'>
-            證   照
+      <div className='flex flex-col w-3/5 pt-4 ml-16'>
+        
+        <div className='flex items-center'>
+            <Button onClick={handleCerClick} 
+            className='flex items-center justify-center w-20 h-8 my-6 font-semibold text-yellow-900 bg-orange-200 rounded'>
+              證   照
+            </Button>           
+          <div className='ml-8'>
+            {isCer ? '未輸入' : (<TextField id="standard-basic" label="Certificate" variant="standard"/>)}
+          </div>
+        </div>   
+        
+        <div className='flex items-center'>
+            <Button onClick={handleLanguageClick} 
+            className='flex items-center justify-center w-20 h-8 my-6 font-semibold text-yellow-900 bg-orange-200 rounded'>
+              語   言
+            </Button>           
+          <div className='ml-8'>
+            {isLanguage ? '未輸入' : (<TextField id="standard-basic" label="Language" variant="standard"/>)}
+          </div>
+        </div>   
+
+        <div className='flex items-center'>
+            <Button onClick={handleEducationalClick} 
+            className='flex items-center justify-center w-20 h-8 my-6 font-semibold text-yellow-900 bg-orange-200 rounded'>
+              學   歷
+            </Button>           
+          <div className='ml-8'>
+            {isEducational ? '未輸入' : (<TextField id="standard-basic" label="Educational" variant="standard"/>)}
+          </div>
+        </div>   
+        
+        <div className='flex items-center'>
+            <Button onClick={handleMajorClick} 
+            className='flex items-center justify-center w-20 h-8 my-6 font-semibold text-yellow-900 bg-orange-200 rounded'>
+              專業能力
+            </Button>           
+          <div className='ml-8'>
+            {isMajor ? '未輸入' : (<TextField id="standard-basic" label="Major" variant="standard"/>)}
           </div>
         </div>     
-        <div className='flex items-center justify-center w-20 h-8 mx-5 my-3 bg-orange-200 rounded'>
-          <div className='font-semibold text-red-950'>
-            語   言
-          </div>
-        </div>   
-        <div className='flex items-center justify-center w-20 h-8 mx-5 my-3 bg-orange-200 rounded'>
-          <div className='font-semibold text-red-950'>
-            學   歷
-          </div>
-        </div>   
-        <div className='flex items-center justify-center w-20 h-8 mx-5 my-3 bg-orange-200 rounded'>
-          <div className='font-semibold text-red-950'>
-            專業能力
-          </div>
-        </div>                                     
       </div>  
+
       <div className='flex justify-center'>
         <div>
-              <Button className='mt-4 w-[480px] font-semibold text-yellow-900 bg-orange-200 '>個人化工作推薦</Button>
+          <Button className='mt-4 w-[480px] font-semibold text-yellow-900 bg-orange-200 '>個人化工作推薦</Button>
         </div>  
-      </div>  
+      </div>
+
     </div>
     
-  )
+  );
 }
 
-export default userdata
+export default Userdata;
