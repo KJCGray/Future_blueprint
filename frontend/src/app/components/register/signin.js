c6d820f48b0438a112e2a1dcabf6e6070cdbd2e2
Yvonne <a0905001923@gmail.com>
2023-12-13 10:49:24 +0800

    logout, signin, signup func

--- frontend/src/app/components/register/signin.js
+++ frontend/src/app/components/register/signin.js
@@ -1,13 +1,22 @@
 "use client";
 import * as React from "react";
+import { useEffect } from "react";
 import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useRouter } from "next/navigation";
 import { useRef } from "react";
+import { setCookie, parseCookies } from "nookies";
+import axios from "axios";
 
-const signin = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+const Signin = () => {
+  const { token } = parseCookies();
+  const { userid } = parseCookies();
+  useEffect(()=>{
+    console.log(token,userid);
+  },[])
   const router = useRouter();
+  const signinNameRef = useRef("");
+  const signinPswdRef = useRef("");
   const theme = createTheme({
     palette: {
       ochre: {
@@ -22,19 +31,47 @@ const signin = () => {
     e.preventDefault();
     router.push("/searchpage");
   }
-  async function login(e){
+  async function login(e) {
     e.preventDefault();
-    
+  }
+  const handleNameChange = (e) => {
+    signinNameRef.current = e.target.value;
+  };
+  const handlePswdChange = (e) => {
+    signinPswdRef.current = e.target.value;
+  };
+  async function signin(e) {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`http://localhost:5000/api/login`, {
+        username: signinNameRef,
+        password: signinPswdRef,
+      });
+      const token = response.data.token;
+      const userid = response.data.userId;
+      setCookie(null, 'token', String(token));
+      setCookie(null, 'userid', userid);
+      console.log(token,userid);
+      router.push("/searchpage");
+      
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <div className="">
-      <form method="post" onSubmit={loginHandler}>
+      <form method="post" onSubmit={signin}>
         <div className="flex items-center justify-center mt-4">
           <label
             htmlFor="user"
             className="flex items-center h-10 text-gray-500 bg-white w-60 rounded-2xl"
           >
-            <input className="pl-4 outline-none" placeholder="使用者名稱" />
+            <input
+              className="pl-4 outline-none"
+              placeholder="使用者名稱"
+              ref={signinNameRef}
+              onChange={handleNameChange}
+            />
           </label>
         </div>
         <div className="flex items-center justify-center mt-6">
@@ -42,7 +79,13 @@ const signin = () => {
             htmlFor="password"
             className="flex items-center h-10 text-gray-500 bg-white w-60 rounded-2xl"
           >
-            <input className="pl-4 outline-none" type="password" placeholder="密碼" />
+            <input
+              className="pl-4 outline-none"
+              type="password"
+              placeholder="密碼"
+              ref={signinPswdRef}
+              onChange={handlePswdChange}
+            />
           </label>
         </div>
         <div className="flex justify-center mt-4">
@@ -63,4 +106,4 @@ const signin = () => {
   );
 };
 
-export default signin;
+export default Signin;
