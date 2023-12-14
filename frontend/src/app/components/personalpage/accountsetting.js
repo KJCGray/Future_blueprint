4260f0850efc6c1d72ea720195c8d5f622de6fdd
PeiRong13 <patty10322213@gmail.com>
2023-12-14 22:13:39 +0800

    personalpage

--- frontend/src/app/components/personalpage/accountsetting.js
+++ frontend/src/app/components/personalpage/accountsetting.js
@@ -37,7 +37,7 @@ const Accountsetting = () => {
                 姓   名
                 </Button>           
                 <div className='ml-8'>
-                    {isName ? '未輸入' : (<TextField id="standard-basic" label="Name" variant="standard"/>)}
+                    {isName ? (<TextField id="standard-basic" label="Name" variant="standard"/>): '未輸入'}
                 </div>
             </div> 
             <div className='flex items-center mt-10'>
@@ -46,7 +46,7 @@ const Accountsetting = () => {
                 信   箱
                 </Button>           
                 <div className='ml-8'>
-                    {isMail ? '未輸入' : (<TextField id="standard-basic" label="Mail" variant="standard"/>)}
+                    {isMail ? (<TextField id="standard-basic" label="Mail" variant="standard"/>): '未輸入'}
                 </div>
             </div> 
             <div className='flex items-center mt-10'>
@@ -55,7 +55,7 @@ const Accountsetting = () => {
                 學   歷
                 </Button>           
                 <div className='ml-8'>
-                    {isEducational ? '未輸入' : (<TextField id="standard-basic" label="Educational" variant="standard"/>)}
+                    {isEducational ?  (<TextField id="standard-basic" label="Educational" variant="standard"/>): '未輸入'}
                 </div>
             </div> 
         </div>
