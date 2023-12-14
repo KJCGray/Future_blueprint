5ff14bbd17b0070f92b804283037d3463fd9fe5e
Yvonne <a0905001923@gmail.com>
2023-12-14 13:09:07 +0800

    search jobs api intergrate

--- frontend/src/app/components/mainpage/reclanguage.js
+++ frontend/src/app/components/mainpage/reclanguage.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-function reclanguage ({Language}) {
+function reclanguage({ Language }) {
   return (
     <div className="overflow-auto border border-solid h-1/3 border-zinc-300">
-    {Language.map((language) => (
-      <div key={language} className="w-auto p-2 m-2 border-b border-zinc-300">{language}</div>
-    ))}
+      {Language.map((Language) => (
+        <div key={Language} className="flex w-auto p-2 m-2 border-b border-zinc-300">
+          <div className="">
+            {Language.language}
+          </div>
+          <div key={Language} className="ml-2">
+            {Language.job_count}
+          </div>
+        </div>
+      ))}
     </div>
   );
-};
+}
 
 export default reclanguage;
--- frontend/src/app/components/mainpage/recommendskills.js
+++ frontend/src/app/components/mainpage/recommendskills.js
@@ -4,8 +4,11 @@ function recommendskills({ Skills }) {
   return (
     <div className="overflow-auto border border-solid h-1/3 border-zinc-300">
       <div>
-        {Skills.map((skill) => (
-          <div key={skill} className="w-auto p-2 m-2 border-b border-zinc-300">{skill}</div>
+        {Skills.map((Skill) => (
+          <div key={Skill} className="flex w-auto p-2 m-2 border-b border-zinc-300">
+            <div>{Skill.skill}</div>
+            <div>{Skill.tool}</div>
+          </div>
         ))}
       </div>
     </div>
