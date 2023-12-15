2808df744ea0cfe6a4c3babc305aacf9f82e3a7e
ruo3531 <ruozhenwork@gmail.com>
2023-12-16 00:42:23 +0800

    push

--- backend/node-express/controllers/userpageController.js
+++ backend/node-express/controllers/userpageController.js
@@ -46,11 +46,11 @@ const PageController = {
     },
     update:(req, res) =>{
 
-        username = req.body.username;
-        token = req.body.token;
+        // username = req.body.username;
+        // token = req.body.token;
         
-        // username = req.session.username;
-        // token = req.session.token;
+        username = req.session.username;
+        token = req.session.token;
 
         userModel.get(token, (err, user) =>{
             if(err) {
