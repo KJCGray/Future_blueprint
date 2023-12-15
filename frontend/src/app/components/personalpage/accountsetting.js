220ff45117178b3bfe7fa8bd781ae87e30199ddf
PeiRong13 <patty10322213@gmail.com>
2023-12-15 23:34:29 +0800

    frontend accountsetting update

--- frontend/src/app/components/personalpage/accountsetting.js
+++ frontend/src/app/components/personalpage/accountsetting.js
@@ -30,23 +30,22 @@ const Accountsetting = () => {
   return (
     <div className=' bg-orange-100 rounded-xl w-[600px] h-[400px]'>
         <div className='flex flex-col w-3/5 pt-4 ml-16'>
-
             <div className='flex items-center mt-8'>
-                <Button onClick={handleNameClick} 
+                <div 
                 className='flex items-center justify-center w-20 h-8 my-6 font-semibold text-yellow-900 bg-orange-200 rounded'>
                 姓   名
-                </Button>           
+                </div>           
                 <div className='ml-8'>
                     {isName ? (<TextField id="standard-basic" label="Name" variant="standard"/>): '未輸入'}
                 </div>
             </div> 
             <div className='flex items-center mt-10'>
-                <Button onClick={handleMailClick} 
+                <div
                 className='flex items-center justify-center w-20 h-8 my-6 font-semibold text-yellow-900 bg-orange-200 rounded'>
                 信   箱
-                </Button>           
+                </div>   
                 <div className='ml-8'>
-                    {isMail ? (<TextField id="standard-basic" label="Mail" variant="standard"/>): '未輸入'}
+                    {isName ? (<TextField id="standard-basic" label="Name" variant="standard"/>): '未輸入'}
                 </div>
             </div> 
             <div className='flex items-center mt-10'>
