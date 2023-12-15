049b7b533bcc8e86373aa9902c9ceadcb370a492
Yvonne <a0905001923@gmail.com>
2023-12-16 01:52:43 +0800

    set forum

---
+++ frontend/src/app/forum/[forumid]/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBar from "../../components/mainpage/searchbar";
+const page = () => {
+  return (
+    <div className="w-screen h-screen m-0 bg-white">
+      <SearchBar />
+      <div className="flex items-center justify-center mt-20"></div>
+    </div>
+  );
+};
+
+export default page;
