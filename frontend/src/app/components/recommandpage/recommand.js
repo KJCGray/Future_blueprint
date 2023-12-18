f87d06390e20b26524ab96d2347f23565af9f94b
PeiRong13 <patty10322213@gmail.com>
2023-12-19 00:06:32 +0800

    frontend update

--- frontend/src/app/components/recommandpage/recommand.js
+++ frontend/src/app/components/recommandpage/recommand.js
@@ -1,21 +1,20 @@
 'use client';
 import React, { useState, useEffect } from "react";
-import {parseCookies} from "nookies";
-import { useRouter } from "next/navigation";
 import axios from "axios";
 
 const Recommand = () => {
-
+  const [certificate,setcertificate]=useState("丙級");
+  const [language,setlanguage]=useState("英文");
+  const [edu,setedu]=useState("輔仁大學");
+  const [major,setmajor]=useState("程式設計");
+  
   async function Joblist() {
     try {
       const response = await axios.post(`http://localhost:5000/api/joblist`, {
-        username: username,
-        token: token,
-        userid:userid,
         certificate:certificate,
-        language:language,
+        language_req:language,
         edu:edu,
-        skill:major,
+        job_skill:major,
       });
       console.log("工作推薦:",response);
     } catch (error) {
@@ -23,12 +22,17 @@ const Recommand = () => {
     }
   }
 
+  useEffect(() => {
+    Joblist();
+  }, []);
+
   return (
     <div className='flex flex-col items-center justify-center w-auto h-auto mb-16'>
         <div className='text-xl font-semibold text-yellow-900'>
           個人化工作推薦
         </div>
         <div className='flex items-center justify-center w-3/5 bg-orange-100 h-96 rounded-xl'>
+        
         </div>
     </div>
   )
